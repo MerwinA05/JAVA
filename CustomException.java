f73905ef4318a57f36ee2988c839dd16d9671421
MerwinA05 <130964315+MerwinA05@users.noreply.github.com>
2023-04-17 13:27:01 +0530

    Custom Exception

---
+++ CustomException.java
@@ -0,0 +1,11 @@
+package Code;
+
+public class CustomException {
+
+	public static void main(String[] args) throws TestException 
+	{
+	  throw new TestException();
+
+	}
+
+}
---
+++ TestException.java
@@ -0,0 +1,7 @@
+package Code;
+
+public class TestException extends Exception
+
+{
+
+}
