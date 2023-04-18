ea988fec40cee944d6d7b19127e942dda74e99b4
MerwinA05 <130964315+MerwinA05@users.noreply.github.com>
2023-04-18 10:06:36 +0530

    Finding the no is Odd or Even

---
+++ OddEven.java
@@ -0,0 +1,22 @@
+package Code;
+
+public class OddEven {
+
+	public static void main(String[] args) 
+	{
+		int num = 1;
+        while(num<=10)
+        {
+		if(num%2 == 0) 
+		{
+		    System.out.println(num + " is even.");
+		} 
+		else 
+		{
+		    System.out.println(num + " is odd.");
+		}
+        num++;
+        }
+	}
+
+}
