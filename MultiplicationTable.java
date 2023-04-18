38275f760ed2134712bddc3aaf941f02a6823759
MerwinA05 <130964315+MerwinA05@users.noreply.github.com>
2023-04-18 10:21:23 +0530

    Multiplication table format

---
+++ MultiplicationTable.java
@@ -0,0 +1,17 @@
+package Loop;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args)
+	{
+		int num = 3;
+
+		for(int i = 1; i <= 10; i++) 
+		{
+		    System.out.println(num + " x " + i + " = " + (num*i));
+		}
+
+
+	}
+
+}
