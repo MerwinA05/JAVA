4057e3a1d7c8f6a4965d7b16ff1cbcc1646bb177
MerwinA05 <130964315+MerwinA05@users.noreply.github.com>
2023-04-18 10:16:16 +0530

    Swaping the two numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+package Code;
+
+public class SwapTwoNumbers 
+{
+	public static void main(String[]args) 
+	{
+		int a = 10;
+	    int b = 20;
+        a = a + b;
+	    b = a - b;
+	    a = a - b;
+		System.out.println("a = " + a); 
+	    System.out.println("b = " + b); 
+    
+  }
+}
